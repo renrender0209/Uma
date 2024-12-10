1790aa262440326a1ca71d49c36ccf87c416845e
Animesh <69345507+n-ce@users.noreply.github.com>
2024-12-10 07:38:22 +0530

    clean all invidious instances

--- updater.ts
+++ updater.ts
@@ -47,11 +47,7 @@ fetch(allPipedInstancesUrl)
       unified: number;
     } = {
       piped: [],
-      invidious: [
-        'https://invidious.technicalvoid.dev',
-        'https://invidious.jing.rocks',
-        'https://invidious.catspeed.cc',
-      ],
+      invidious: [],
       hyperpipe: [],
       cobalt: 'https://cobalt-api.kwiatekmiki.com',
       proxy: 'https://invidious.adminforge.de',
