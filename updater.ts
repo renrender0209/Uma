15839136a8a170ecea91cbdee78902df2281f952
Animesh <69345507+n-ce@users.noreply.github.com>
2024-12-14 16:36:24 +0530

    Update updater.ts

--- updater.ts
+++ updater.ts
@@ -53,7 +53,7 @@ fetch(allPipedInstancesUrl)
     } = {
       piped: [],
       invidious: [],
-      cobalt: 'https://cobalt-backend.canine.tools',
+      cobalt: 'https://api.co.rooot.gay',
       proxy: 'https://invidious.adminforge.de',
       fallback: 'https://video-api-transform.vercel.app/api'
     };
