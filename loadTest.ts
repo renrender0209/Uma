2900ed35ee8ea9abaf43084aad60adb5c889f29c
Animesh <69345507+n-ce@users.noreply.github.com>
2024-12-27 18:26:14 +0530

    Create loadTest.ts

---
+++ loadTest.ts
@@ -0,0 +1,32 @@
+export async function loadAndTest(i: string): Promise<string | null> {
+  
+  const url = await fetch(`${i}/api/v1/videos/LuoB1OQvjqk`)
+    .then(res => res.json())
+    .then(data => {
+      if (data && 'adaptiveFormats' in data)
+        return data;
+      else throw new Error(data.error);
+    })
+    .then(
+      (data: {
+        adaptiveFormats: {
+          url: string,
+          type: string
+        }[]
+      }) => (data.adaptiveFormats
+        .filter((f) => f.type.startsWith('audio'))
+      [0].url)
+    );
+
+  if (!url) return '';
+
+  const curl = new URL(url);
+  const origin = curl.origin;
+  const proxiedUrl = url.replace(origin, i) + '&host=' + origin.slice(8);
+
+  const passed = await fetch(proxiedUrl)
+    .then(res => res.ok);
+
+  return passed ? i : '';
+
+}
