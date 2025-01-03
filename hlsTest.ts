4a350a4312c3ebf1ced32ff8d886469d191ca14f
Animesh <69345507+n-ce@users.noreply.github.com>
2025-01-03 13:17:39 +0530

    Update hlsTest.ts

--- hlsTest.ts
+++ hlsTest.ts
@@ -1,15 +1,10 @@
-export async function hlsTest(i: string): Promise<string | null> {
-  
-  const hls = await fetch(`${i}/streams/LuoB1OQvjqk`)
+export const hlsTest = async (i: string): Promise<string | null> =>
+  fetch(`${i}/streams/LuoB1OQvjqk`)
     .then(res => res.json())
     .then(data => {
-      console.log(data.hls || i)
-      if (data && 'hls' in data)
+      console.log(data.hls);
+      if (data && data.hls)
         return data.hls;
       else throw new Error(data.error);
     })
   .catch(() => '');
-
-  return hls ? i : '';
-
-}
