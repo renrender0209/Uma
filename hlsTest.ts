0f1c937be506e1dac7fd85136b71e64ed3c4f52d
Animesh <69345507+n-ce@users.noreply.github.com>
2025-01-16 13:55:01 +0530

    Update hlsTest.ts

--- hlsTest.ts
+++ hlsTest.ts
@@ -2,7 +2,7 @@ export const hlsTest = async (i: string): Promise<string | null> =>
   fetch(`${i}/streams/LuoB1OQvjqk`)
     .then(res => res.json())
     .then(data => {
-      console.log(i, `hls: ${Boolean(data.hls)}`)
+      console.log(i, `data: ${Boolean(data.audioStreams.length)}, hls: ${Boolean(data.hls)}`)
       if (data.hls)
         return i;
       else throw new Error(data.error);
