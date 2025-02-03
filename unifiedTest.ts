745a99d9b0a7be8bf2bca2fb0c4b06c19acc7abc
Animesh <69345507+n-ce@users.noreply.github.com>
2025-02-03 13:01:41 +0530

    Create unifiedTest.ts

---
+++ unifiedTest.ts
@@ -0,0 +1,25 @@
+export async function unifiedTest(i: string, u: string): Promise<string | null> {
+
+  const url = await fetch(`${i}/streams/LuoB1OQvjqk`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(i, `data: ${'audioStreams' in data}, hls: ${Boolean(data.hls)}`)
+      if (data.audioStreams.length)
+        return data.audioStreams[0].url;
+      else throw new Error(data.error);
+    })
+  .catch(() => '');
+
+  if (!url) return '';
+
+  const curl = new URL(url);
+  const origin = curl.origin;
+  const unifiedUrl = url.replace(origin, u);
+
+  const passed = await fetch(proxiedUrl)
+    .then(res => res.status === 200)
+    .catch(() => false);
+
+  return passed ? i : '';
+
+}
